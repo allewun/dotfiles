ec24d9ce356234589bbd1f4665faab519be9ef77
Allen Wu <allewun@yahoo.com>
2013-10-30 02:45:54 -0700

    Added wifi toggling script

---
+++ scripts/toggle-wifi.sh
@@ -0,0 +1,6 @@
+# http://apple.stackexchange.com/a/37216
+
+device=$(networksetup -listallhardwareports |
+grep -E '(Wi-Fi|AirPort)' -A 1 | grep -o en.)
+[[ "$(networksetup -getairportpower $device)" == *On ]] && v=off || v=on
+networksetup -setairportpower $device $v
