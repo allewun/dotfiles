90c988563aa82a427e2ae34bf574e068f56b4de5
Allen Wu <allewun@yahoo.com>
2013-11-10 23:59:03 -0800

    [setup] added history stuff

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -11,9 +11,11 @@ DOTFILES_OLD_REG=($(comm -12 <(find $DOTFILE_PATH -name ".*" -type f -maxdepth 1
 DOTFILES_OLD_SYM=($(comm -12 <(find $DOTFILE_PATH -name ".*" -type f -maxdepth 1 -exec basename {} \;) \
                              <(find $HOME         -name ".*" -type l -maxdepth 1 -exec basename {} \;)))
 DROPBOX_HISTORY_PATH=~/Dropbox/history
+HISTORY_FILE=$HOME/.zsh_history
+DATE=($(date +%Y%m%d%H%M%S))
+
 
 # backup old dotfiles (regular files)
-DATE=($(date +%Y%m%d%H%M%S))
 BACKUP_PATH="$HOME/dotfiles-$DATE"
 if [[ -n $DOTFILES_OLD_REG ]]; then
   mkdir $BACKUP_PATH && echo -e "
@@ -53,9 +55,21 @@ echo -e
 # setup .zsh_history symlink
 echo "Name of file that ~/.zsh_history will be symlinked to: "
 read HISTORY_FILE_NAME
+echo -e
 
-(mkdir -p "$DROPBOX_HISTORY_PATH" && mv "$HOME/.zsh_history" $_) || touch "$DROPBOX_HISTORY_PATH/$HISTORY_FILE_NAME"
-ln -s "$DROPBOX_HISTORY_PATH/$HISTORY_FILE_NAME" "$HOME/.zsh_history"
+mkdir -p "$DROPBOX_HISTORY_PATH"
+
+# if a history file (non-symlink) already exists, back it up
+# otherwise create the backup and the history file
+if [[ -f $HISTORY_FILE && ! -h $HISTORY_FILE ]]; then
+  mv "$HISTORY_FILE" "$DROPBOX_HISTORY_PATH/.zsh_history-$DATE" && echo -e "Backed-up old history file to .zsh_history-$DATE"
+elif [[ ! -f $HISTORY_FILE ]]; then
+  touch "$DROPBOX_HISTORY_PATH/$HISTORY_FILE_NAME" "$HISTORY_FILE"
+fi
+
+# update the symlink
+ln -sf "$DROPBOX_HISTORY_PATH/$HISTORY_FILE_NAME" "$HISTORY_FILE" && echo -e "Linked: .zsh_history -> $DROPBOX_HISTORY_PATH/$HISTORY_FILE_NAME"
+
 
   echo -e "
 *-----------------------------*
