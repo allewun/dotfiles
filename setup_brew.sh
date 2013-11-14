d626c3f5a2a84c7f1f1e8884a83d6f4d0134cbb7
Allen Wu <allen.wu@originate.com>
2013-11-14 13:23:19 -0800

    [brew] added git-extras

--- setup_brew.sh
+++ setup_brew.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+brew update
+brew upgrade
+
 brew install ack
 brew install wget
 brew install coreutils
@@ -8,3 +11,4 @@ brew install imagemagick
 brew install tmux
 brew install watch
 brew install tig
+brew install git-extras
