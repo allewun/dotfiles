86341154564a99a01bc70a4aa566201b95ddbcdf
Allen Wu <allewun@yahoo.com>
2013-11-13 23:26:02 -0800

    [setup] renamed .osx and added backup to vim setup

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,6 +1,15 @@
 #!/bin/zsh
 
+#=========================
+# Set up .vim folder
+#=========================
+
 DOTFILE_PATH=~/dotfiles
+DATE=($(date +%Y%m%d%H%M%S))
+
+# backup source history file if it exists already
+if [[ -d $HOME/.vim ]]; then
+  mv "$HOME/.vim" "$HOME/.vim-$DATE" && echo "Backed-up old .vim directory to $HOME/.vim-$DATE"
+fi
 
 gcp -rs $DOTFILE_PATH/vim $HOME/.vim
-
