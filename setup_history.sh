33d2124908fd669bdc9d5a7b912fb346d3d214ce
Allen Wu <allen.wu@ucla.edu>
2013-11-22 00:45:02 -0800

    [setup] changed history file symlink setup (dropbox -> original, rather than reverse)

--- setup_history.sh
+++ setup_history.sh
@@ -10,27 +10,19 @@ HISTORY_FILE=$HOME/.zsh_history
 DATE=($(date +%Y%m%d%H%M%S))
 
 # setup .zsh_history symlink
-echo "Name of file that ~/.zsh_history will be symlinked to: "
+echo "Name of backup file that will symlink to ~/.zsh_history: "
 read HISTORY_FILE_NAME
 echo
 
 mkdir -p "$DROPBOX_HISTORY_PATH"
 
-# if a history file (non-symlink) already exists, back it up
-# otherwise create the backup and the history file
-if [[ -f $HISTORY_FILE && ! -h $HISTORY_FILE ]]; then
-  mv "$HISTORY_FILE" "$DROPBOX_HISTORY_PATH/.zsh_history-$DATE" && echo "Backed-up old history file to .zsh_history-$DATE"
-elif [[ ! -f $HISTORY_FILE ]]; then
-  touch "$DROPBOX_HISTORY_PATH/$HISTORY_FILE_NAME" "$HISTORY_FILE"
+# create history file if it doesn't exist
+if [[ ! -f $HISTORY_FILE ]]; then
+  touch "$HISTORY_FILE"
 fi
 
-# backup source history file if it exists already
-if [[ -f $DROPBOX_HISTORY_PATH/$HISTORY_FILE_NAME ]]; then
-  mv "$DROPBOX_HISTORY_PATH/$HISTORY_FILE_NAME" "$DROPBOX_HISTORY_PATH/$HISTORY_FILE_NAME-$DATE" && echo "Backed-up old source history file to $DROPBOX_HISTORY_PATH/$HISTORY_FILE_NAME-$DATE"
-fi
-
-# update the symlink
-ln -sf "$DROPBOX_HISTORY_PATH/$HISTORY_FILE_NAME" "$HISTORY_FILE" && echo "Linked: .zsh_history -> $DROPBOX_HISTORY_PATH/$HISTORY_FILE_NAME"
+# update the symlink (Dropbox symlinks to ~/.zsh_history; Dropbox follows symlinks and saves the contents, rather than just the reference)
+ln -sf "$HISTORY_FILE" "$DROPBOX_HISTORY_PATH/$HISTORY_FILE_NAME" && echo "Linked: $DROPBOX_HISTORY_PATH/$HISTORY_FILE_NAME -> ~/.zsh_history"
 
 echo "
 *-----------------------------*
