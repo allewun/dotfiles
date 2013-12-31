7c9dc83e231d9392e3d33f46e2e1007d97256b1e
Allen Wu <allen.wu@ucla.edu>
2013-12-30 16:42:19 -0800

    [prefs] added keyremap4macbook and pckeyboardhack settings

---
+++ preferences/KeyRemap4MacBook/kr4mb-export.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# run this to import settings
+
+cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
+
+$cli set repeat.wait 20
+/bin/echo -n .
+$cli set remap.fn2controlL 1
+/bin/echo -n .
+$cli set repeat.initial_wait 333
+/bin/echo -n .
+$cli set remap.controlDelete2forwarddelete 1
+/bin/echo -n .
+$cli set caps_lock_to_hyper 1
+/bin/echo -n .
+$cli set remap.controlL2fn 1
+/bin/echo -n .
+/bin/echo
