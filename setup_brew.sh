14522a0a9fbfb227ce3d8321626b39ed3002695e
Allen Wu <allen.wu@ucla.edu>
2014-01-10 20:56:51 -0800

    [brew] ffmpeg, mysql, p7zip

--- setup_brew.sh
+++ setup_brew.sh
@@ -13,6 +13,9 @@ brew install watch
 brew install tig
 brew install git-extras
 brew install terminal-notifier
+brew install ffmpeg
+brew install mysql
+brew install p7zip
 brew install rbenv ruby-build rbenv-gem-rehash
 
 brew tap homebrew/versions
