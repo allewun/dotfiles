541bdecf732c7b34c98da47cd78bd7420de4887f
Allen Wu <allen.wu@ucla.edu>
2014-01-13 15:30:42 -0800

    [zsh][scripts] suppressed gemset prompt when none exists

--- scripts/rbenv-prompt.sh
+++ scripts/rbenv-prompt.sh
@@ -3,9 +3,9 @@
 function __rbenv_ps1() {
   if rbenv version > /dev/null 2>&1; then
     local VERSION=$(rbenv version-name)
-    local GEMSETS=$(rbenv gemset active | sed "s/[[:<:]]global[[:>:]]//g" | sed -E "s/^ +| +$//g" | sed -E "s/ +/, /g")
 
     if rbenv version | fgrep -q '.ruby-version'; then
+      local GEMSETS=$(rbenv gemset active 2>/dev/null | sed "s/[[:<:]]global[[:>:]]//g" | sed -E "s/^ +| +$//g" | sed -E "s/ +/, /g")
       echo " [$VERSION$( [[ -n $GEMSETS ]] && echo " / $GEMSETS" )]"
       return 0
     fi
