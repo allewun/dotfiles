7b47f3b26734c47b14b6caeda287b0f0c115dc35
Allen Wu <allen.wu@ucla.edu>
2014-01-17 18:34:54 -0800

    [prefs] added shift+shift -> caps lock and cmd+cmd -> f18 (for totalterminal)
    totalterminal's cmd+cmd was getting buggy for some reason

--- preferences/KeyRemap4MacBook/kr4mb-export.sh
+++ preferences/KeyRemap4MacBook/kr4mb-export.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 
+# run this to export settings and replace this file
+# /Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli export > ~/dotfiles/preferences/KeyRemap4MacBook/kr4mb-export.sh
+
 # run this to import settings
+# . ~/dotfiles/preferences/KeyRemap4MacBook/kr4mb-export.sh
 
 cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
 
 $cli set repeat.wait 20
 /bin/echo -n .
-$cli set remap.fn2controlL 1
-/bin/echo -n .
 $cli set repeat.initial_wait 333
 /bin/echo -n .
-$cli set remap.controlDelete2forwarddelete 1
+$cli set double_tap_shift_to_caps_lock 1
 /bin/echo -n .
 $cli set caps_lock_to_hyper 1
+/bin/echo -n .
+$cli set remap.fn2controlL 1
 /bin/echo -n .
 $cli set remap.controlL2fn 1
 /bin/echo -n .
 $cli set remap.eject2forwarddelete 1
+/bin/echo -n .
+$cli set double_tap_command_to_f18 1
+/bin/echo -n .
+$cli set parameter.doublepressmodifier_threshold 200
 /bin/echo -n .
 /bin/echo
