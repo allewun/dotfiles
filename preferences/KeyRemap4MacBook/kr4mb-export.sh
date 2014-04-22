602fca94c71ffefcab70dabe7679f93304cfda8c
Allen Wu <allen.wu@ucla.edu>
2014-04-21 20:12:54 -0700

    [kr4m] Fn-Functional key -> F1..F12

--- preferences/KeyRemap4MacBook/kr4mb-export.sh
+++ preferences/KeyRemap4MacBook/kr4mb-export.sh
@@ -8,22 +8,34 @@
 
 cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
 
-$cli set repeat.wait 20
+$cli set remap.fn_consumer_to_fkeys_f5 1
 /bin/echo -n .
 $cli set repeat.initial_wait 333
 /bin/echo -n .
+$cli set parameter.doublepressmodifier_threshold 200
+/bin/echo -n .
+$cli set remap.fn_consumer_to_fkeys_f7 1
+/bin/echo -n .
 $cli set double_tap_shift_to_caps_lock 1
+/bin/echo -n .
+$cli set remap.controlL2fn 1
+/bin/echo -n .
+$cli set double_tap_command_to_f18 1
+/bin/echo -n .
+$cli set remap.fn_consumer_to_fkeys_f10 1
+/bin/echo -n .
+$cli set remap.fn_consumer_to_fkeys_f1 1
+/bin/echo -n .
+$cli set repeat.wait 20
+/bin/echo -n .
+$cli set remap.fn_consumer_to_fkeys_f3 1
 /bin/echo -n .
 $cli set caps_lock_to_hyper 1
 /bin/echo -n .
 $cli set remap.eject2forwarddelete 1
 /bin/echo -n .
-$cli set remap.controlL2fn 1
+$cli set double_tap_alt_to_f17 1
 /bin/echo -n .
 $cli set remap.fn2controlL 1
-/bin/echo -n .
-$cli set double_tap_command_to_f18 1
-/bin/echo -n .
-$cli set parameter.doublepressmodifier_threshold 200
 /bin/echo -n .
 /bin/echo
