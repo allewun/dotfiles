a3a9123fa9d988cbc0903de67071f564d365ff33
Allen Wu <allen.wu@ucla.edu>
2014-05-03 23:46:07 -0700

    [setup] added setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+autoload -U colors && colors
+
+echo "$fg[cyan]Sourcing .zshrc...$reset_color"      && source .zshrc
+echo "$fg[cyan]Setting up brew...$reset_color"      && ./setup_brew.sh
+echo "$fg[cyan]Setting up dotfiles...$reset_color"  && ./setup_dotfiles.sh
+echo "$fg[cyan]Setting up vim...$reset_color"       && ./setup_vim.sh
+echo "$fg[cyan]Setting up osx...$reset_color"       && ./setup_osx.sh
+echo "$fg[cyan]Setting up quicklook...$reset_color" && ./setup_quicklook.sh
+echo "$fg[cyan]Setting up history...$reset_color"   && ./setup_history.sh
