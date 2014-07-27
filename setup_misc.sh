9b1fe573cc35ef126fb983cfdff131e990a9a6e5
Allen Wu <allen.wu@originate.com>
2014-07-27 03:37:34 -0700

    [fonts] added inconsolata-aw variants to setup

--- setup_misc.sh
+++ setup_misc.sh
@@ -19,4 +19,6 @@
 
   # install custom fonts
   cp "$DOTFILE_PATH/misc/fonts/Inconsolata-aw/Inconsolata-aw.otf" ~/Library/Fonts && echo "Installed Inconsolata-aw font"
+  cp "$DOTFILE_PATH/misc/fonts/Inconsolata-aw/Inconsolata-aw-bold.otf" ~/Library/Fonts && echo "Installed Inconsolata-aw bold font"
+  cp "$DOTFILE_PATH/misc/fonts/Inconsolata-aw/Inconsolata-aw-italic.otf" ~/Library/Fonts && echo "Installed Inconsolata-aw italic font"
 )
