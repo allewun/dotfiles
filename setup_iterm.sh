13f738b84f75a7ac5503dcb3aa91a2ba80fc55fa
Allen Wu <allen.wu@originate.com>
2015-02-02 14:46:59 -0800

    [iTerm] Load settings fix

--- setup_iterm.sh
+++ setup_iterm.sh
@@ -5,9 +5,18 @@
 #=========================
 
 (
+echo "NOTE: iTerm needs to be closed"
+
 # Remove iTerm from dock
 /usr/libexec/PlistBuddy -c 'Add :LSUIElement bool true' /Applications/iTerm.app/Contents/Info.plist
 
 # Restore iTerm in dock
 # /usr/libexec/PlistBuddy -c 'Delete :LSUIElement' /Applications/iTerm.app/Contents/Info.plist
+
+# Copy iTerm settings
+cp -f "$DOTFILE_PATH/preferences/iTerm/com.googlecode.iterm2.plist" ~/Library/Preferences/ && echo "Restored iTerm settings"
+
+# Reload preferences (http://apple.stackexchange.com/questions/111534/iterm2-doesnt-read-com-googlecode-iterm2-plist)
+defaults read com.googlecode.iterm2
+killall cfprefsd
 )
