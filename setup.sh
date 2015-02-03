1ed017e977d3a9fde7db51fa313569f0621c2577
Allen Wu <allen.wu@originate.com>
2015-02-02 17:03:13 -0800

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env zsh
 
+echo "Ensure that .zshrc has been sourced first. Press any key to continue."
+read
+
 autoload -U colors && colors
 
-echo "$fg[cyan]Sourcing .zshrc...$reset_color"      && source .zshrc
 echo "$fg[cyan]Setting up brew...$reset_color"      && ./setup_brew.sh
 echo "$fg[cyan]Setting up dotfiles...$reset_color"  && ./setup_dotfiles.sh
 echo "$fg[cyan]Setting up vim...$reset_color"       && ./setup_vim.sh
