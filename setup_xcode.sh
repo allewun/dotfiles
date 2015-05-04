c40151e3d496bd17c6f10a10a357eb1ebf98844c
Allen Wu <allen.wu@originate.com>
2015-05-03 18:15:34 -0700

    [xcode] improve formatting

--- setup_xcode.sh
+++ setup_xcode.sh
@@ -9,20 +9,22 @@
   XCODE_CURSOR_PATH=/Applications/Xcode.app/Contents/SharedFrameworks/DVTKit.framework/Resources
   sudo cp "$XCODE_CURSOR_PATH/DVTIbeamCursor.tiff" "$XCODE_CURSOR_PATH/DVTIbeamCursor-backup.tiff"
   sudo cp -f "$DOTFILE_PATH/misc/cursors/DVTIbeamCursor.tiff" "$XCODE_CURSOR_PATH/"
-  echo "Fixed ugly/barely-visible xcode i-beam cursor\n"
+  echo "* Fixed ugly/barely-visible xcode i-beam cursor"
+
+  # install color theme
+  cp "$DOTFILE_PATH/xcode/Made of Code.dvtcolortheme" ~/Library/Developer/Xcode/UserData/FontAndColorThemes
+  echo "* Installed Made of Code color theme (~/Library/Developer/Xcode/UserData/FontAndColorThemes)"
 
   # install xcode snippets
   local randomtempdir="$(randomtempdir)"
   mkdir "$randomtempdir"
   mv ~/Library/Developer/Xcode/UserData/CodeSnippets/* "$randomtempdir"
-  echo "Old snippets temporarily backed up to $randomtempdir" | indent
+  echo "* Installing Xcode snippets"
+  echo "* Old snippets temporarily backed up to $randomtempdir" | indent 2
 
   for snippet in $DOTFILE_PATH/xcode/snippets/*; do
-    echo "Installing snippet \"$(basename $snippet)\""
-    xcodesnippet install $snippet | indent
+    echo "* Installing snippet \"$(basename $snippet)\"" | indent 4
+    xcodesnippet install $snippet | indent 6
+    echo
   done
-
-  # install color theme
-  cp "$DOTFILE_PATH/xcode/Made of Code.dvtcolortheme" ~/Library/Developer/Xcode/UserData/FontAndColorThemes
-  echo "Installed Made of Code color theme"
 )
