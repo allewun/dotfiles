fffc443ae99725e11354ad2f7d0470af21d4424b
Allen Wu <allenwu@fb.com>
2019-12-02 17:11:28 -0800

    [xcode] symlinks don’t work, xcode replaces it with regular file on write

--- setup/xcode.sh
+++ setup/xcode.sh
@@ -3,16 +3,14 @@
 source $DOTFILE_PATH/zsh/functions.sh
 
 (
+  THEME_SRC=$DOTFILE_PATH/xcode/themes/
   THEME_DEST=~/Library/Developer/Xcode/UserData/FontAndColorThemes
 
   # install color themes
   mkdir -p $THEME_DEST
 
-  for file in $DOTFILE_PATH/xcode/themes/*; do
-    BASENAME=$(basename $file)
-    DESTINATION_FILE="$THEME_DEST/$BASENAME"
-
-    echo "* Installing $BASENAME..."
-    (ln -s "$file" "$DESTINATION_FILE" > /dev/null 2>&1) && echo "    Linked: $DESTINATION_FILE" || (ln -sf "$file" "$DESTINATION_FILE" && echo "    Relinked: $DESTINATION_FILE")
+  for file in $THEME_SRC/*; do
+    echo "* Installing $(basename $file)..."
+    cp -v $file $THEME_DEST
   done
 )
