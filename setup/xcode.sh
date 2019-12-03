fb5bd8a92e168e3a3ec1a692e9e1888205785233
Allen Wu <allenwu@fb.com>
2019-12-02 17:04:15 -0800

    [xcode] use symlinks for themes

--- setup/xcode.sh
+++ setup/xcode.sh
@@ -3,10 +3,16 @@
 source $DOTFILE_PATH/zsh/functions.sh
 
 (
-  THEME_DESTINATION=~/Library/Developer/Xcode/UserData/FontAndColorThemes
+  THEME_DEST=~/Library/Developer/Xcode/UserData/FontAndColorThemes
 
-  # install color theme
-  mkdir -p $THEME_DESTINATION
-  cp "$DOTFILE_PATH/xcode/Made of Code.dvtcolortheme" $THEME_DESTINATION
-  echo "* Installed Made of Code color theme ($THEME_DESTINATION)"
+  # install color themes
+  mkdir -p $THEME_DEST
+
+  for file in $DOTFILE_PATH/xcode/themes/*; do
+    BASENAME=$(basename $file)
+    DESTINATION_FILE="$THEME_DEST/$BASENAME"
+
+    echo "* Installing $BASENAME..."
+    (ln -s "$file" "$DESTINATION_FILE" > /dev/null 2>&1) && echo "    Linked: $DESTINATION_FILE" || (ln -sf "$file" "$DESTINATION_FILE" && echo "    Relinked: $DESTINATION_FILE")
+  done
 )
