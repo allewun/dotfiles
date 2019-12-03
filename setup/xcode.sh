b2809d1294da64ac44394c61a8198646b20bd27b
Allen Wu <allenwu@fb.com>
2019-12-02 16:46:56 -0800

    [xcode] fix setup

--- setup/xcode.sh
+++ setup/xcode.sh
@@ -3,7 +3,10 @@
 source $DOTFILE_PATH/zsh/functions.sh
 
 (
+  THEME_DESTINATION=~/Library/Developer/Xcode/UserData/FontAndColorThemes
+
   # install color theme
-  cp "$DOTFILE_PATH/xcode/Made of Code.dvtcolortheme" ~/Library/Developer/Xcode/UserData/FontAndColorThemes
-  echo "* Installed Made of Code color theme (~/Library/Developer/Xcode/UserData/FontAndColorThemes)"
+  mkdir -p $THEME_DESTINATION
+  cp "$DOTFILE_PATH/xcode/Made of Code.dvtcolortheme" $THEME_DESTINATION
+  echo "* Installed Made of Code color theme ($THEME_DESTINATION)"
 )
