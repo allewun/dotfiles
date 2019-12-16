4bcb4a645a99094cf92f8db3a4218d1cfaaebe36
Allen Wu <allenwu@fb.com>
2019-12-16 09:18:53 -0800

    [setup] fix dotfile backup functionality

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -5,22 +5,21 @@
 #==========================
 
 (
-DOTFILES_NEW=($(find $DOTFILE_PATH -not -name '.DS_Store' -name ".*" -type f -maxdepth 1 -exec basename {} \;))
-DOTFILES_OLD_REG=($(comm -12 <(find $HOME         -not -name '.DS_Store' -name ".*" -type f -maxdepth 1 -exec basename {} \;) \
-                             <(find $DOTFILE_PATH -not -name '.DS_Store' -name ".*" -type f -maxdepth 1 -exec basename {} \;)))
-DATE=($(date +%Y%m%d%H%M%S))
-
+DOTFILES_OLD=$(find $HOME         -not -name '.DS_Store' -name ".*" -type f -maxdepth 1 -exec basename {} \; | sort)
+DOTFILES_NEW=$(find $DOTFILE_PATH -not -name '.DS_Store' -name ".*" -type f -maxdepth 1 -exec basename {} \; | sort)
+DOTFILES_TO_BACKUP=($(comm -12 <(echo "$DOTFILES_OLD") <(echo "$DOTFILES_NEW")))
+DATE=$(date +%Y%m%d%H%M%S)
 
 # backup old dotfiles (regular files)
 BACKUP_PATH="$HOME/dotfiles-$DATE"
-if [[ -n $DOTFILES_OLD_REG ]]; then
+if [[ -n $DOTFILES_TO_BACKUP ]]; then
   mkdir $BACKUP_PATH && echo "
 *-----------------------------*
 |  Old dotfiles backed-up to  |
 |  ~/dotfiles-$DATE  |
 *-----------------------------*\n"
 
-  for i in $DOTFILES_OLD_REG; do
+  for i in $DOTFILES_TO_BACKUP; do
     mv $HOME/$i $BACKUP_PATH/$i
     echo "Backed-up old dotfile: $i"
   done
@@ -34,7 +33,7 @@ fi
 echo
 
 # symlink new dotfiles
-for i in $DOTFILES_NEW; do
+for i in $(echo $DOTFILES_NEW); do
   f=$(basename $i)
   (ln -s "$DOTFILE_PATH/$f" "$HOME/$f" > /dev/null 2>&1) && echo "Linked: ~/$f" || (ln -sf "$DOTFILE_PATH/$f" "$HOME/$f" && echo "Relinked: $f")
 done
