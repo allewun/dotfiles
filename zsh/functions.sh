83e8680768052246c70b8f7c7506acdf5f781eb0
Allen Wu <allewun@gmail.com>
2020-04-29 12:51:29 -0700

    [zsh] safer history

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -9,7 +9,7 @@ function hist() {
   local none=$'\033[0m'
 
   echo "${magenta}$(wc -l "$HISTFILE" | sed -e 's/^[ ]*//')${none}"
-  history -nt '%F %T' 1 | sed -E "s/^([0-9: -]{19})(.*)$/${yellow}[\1]${none}\2/g" | less +G
+  \history -nt '%F %T' 1 | sed -E "s/^([0-9: -]{19})(.*)$/${yellow}[\1]${none}\2/g" | less +G
 }
 
 
