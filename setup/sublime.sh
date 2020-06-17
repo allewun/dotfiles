485ecc7aa0263c8204781dbd7443152138f1fc52
Allen Wu <allewun@gmail.com>
2020-06-17 15:33:33 -0700

    [sublime] guard setup

--- setup/sublime.sh
+++ setup/sublime.sh
@@ -5,10 +5,20 @@ if [[ "$OSTYPE" != "darwin"* ]]; then
     exit
 fi
 
+if [[ ! -a "/Applications/Sublime Text.app" ]]; then
+    echo "Skipping Sublime setup (not yet installed)."
+    echo "  * https://www.sublimetext.com/"
+    exit
+fi
+
 source "$DOTFILE_PATH/zsh/functions.sh"
 
 SUBLIME_SRC="$DOTFILE_PATH/preferences/Sublime"
 SUBLIME_DEST="$HOME/Library/Application Support/Sublime Text 3/Packages"
+
+# symlink subl command
+echo "Symlinking subl command..."
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
 
 # theme
 echo "Installing Soda Theme..."
