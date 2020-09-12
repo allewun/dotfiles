1cb24eeed5dace782470cac6839cd0911f80757c
Allen Wu <allewun@gmail.com>
2020-09-11 21:58:41 -0700

    [setup] tip

--- setup/misc.sh
+++ setup/misc.sh
@@ -7,4 +7,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     cp "$DOTFILE_PATH/misc/fonts/Inconsolata-aw/Inconsolata-aw-italic.otf" ~/Library/Fonts && echo "Installed Inconsolata-aw italic font"
 fi
 
+# install tip scripts
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    cp "$DOTFILE_PATH/scripts/tip-provider.rb" ~/Library/Application\ Scripts/tanin.tip/provider.script && echo "Synced Tip script"
+fi
+
 echo "✅ Done."
